783dd215a6c8a19d0078d2a4931e320fede8786d
Nozadze Otto <135124962+NozadzeOtto@users.noreply.github.com>
2023-06-05 12:24:33 +0400

    Add files via upload

---
+++ Assignment1/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Sheiyvanet temperatura: ");
+        string input = Console.ReadLine();
+        int number = int.Parse(input);
+
+        if (number < 0)
+        {
+            Console.WriteLine("yinavs");
+        }
+        else if (number > 0 && number <= 30)
+        {
+            Console.WriteLine("kargi amindia");
+        }
+        else
+        {
+            Console.WriteLine("cxela");
+        }
+    }
+}
