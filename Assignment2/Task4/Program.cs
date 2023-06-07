34411d3f3415f661a2263d92a9eb5b3759766cc5
NozadzeOtto <nozadzeoto3@gmail.com>
2023-06-07 15:29:27 +0400

    Edits for final result

--- Assignment2/Task4/Program.cs
+++ Assignment2/Task4/Program.cs
@@ -2,23 +2,22 @@
 {
     private static void Main(string[] args)
     {
-        while (true)
+
+        Console.WriteLine("height: ");
+        int number = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= number; i++)
         {
-            Console.WriteLine("height: ");
-            int number = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= number; i++)
+            string line = "";
+            for (int k = 1; k <= number - i; k++) line += ' ';
+            for (int j = 0; j < i; j++)
             {
-                string line = "";
-                for (int k = 1; k <= number - i; k++) line += ' ';
-                for (int j = 0; j < i; j++)
-                {
 
-                    if (j > 0) line += "**";
-                    else { line += '*'; };
+                if (j > 0) line += "**";
+                else { line += '*'; };
 
-                }
-                Console.WriteLine(line);
             }
+            Console.WriteLine(line);
         }
+        
     }
 }
