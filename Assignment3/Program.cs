7052ca45e372cf7b66785cfc42a64c4414af203a
NozadzeOtto <nozadzeoto3@gmail.com>
2023-06-08 15:53:12 +0400

    Assignment3

---
+++ Assignment3/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("sheiyvanet masivis zoma: ");
+        int size = int.Parse(Console.ReadLine());
+        int[] array = new int[size];
+
+        Console.WriteLine("sheiyvanet " + size + " elementi: ");
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+
+        }
+
+        for (int k = 0; k < size; k++)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                int tempVar;
+
+                if (i < array.Length - 1 && array[i] > array[i + 1])
+                {
+                    tempVar = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = tempVar;
+                };
+            }
+        }
+
+
+        //printing
+        string result = "";
+        foreach (int i in array)
+            result += i + ", ";
+        Console.WriteLine(result);
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static int bigger;
+
+    private static void Main(string[] args)
+    {
+        //first array
+        Console.WriteLine("sheiyvanet masivis zoma: ");
+        int size = int.Parse(Console.ReadLine());
+        string[] arrayString = new string[size];
+
+        Console.WriteLine("sheiyvanet " + size + " elementi: ");
+        for (int i = 0; i < arrayString.Length; i++) arrayString[i] = Console.ReadLine();
+        
+        
+        //second array
+        Console.WriteLine("sheiyvanet meore masivis zoma: ");
+        int size2 = int.Parse(Console.ReadLine());
+        int[] arrayInt = new int[size2];
+
+        Console.WriteLine("sheiyvanet " + size2 + " ricxvi: ");
+        for (int i = 0; i < arrayInt.Length; i++) arrayInt[i] = int.Parse(Console.ReadLine());
+
+        int biggerIndex = size > size2 ? size : size2;
+
+        string[] resultArray = new string[biggerIndex*2];
+        for (int i = 0; i < biggerIndex; i++)
+        {
+            if(i<arrayString.Length)resultArray[i*2] = arrayString[i];
+            if(i<arrayInt.Length)resultArray[i*2 + 1] = arrayInt[i].ToString();
+        }
+
+
+        //Printing
+        string result = "";
+        for (int i = 0; i < resultArray.Length; i++)
+            result += resultArray[i] + ", ";
+        Console.WriteLine(result);
+    }
+}
