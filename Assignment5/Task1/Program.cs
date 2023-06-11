3b8c50749c3a993f3c92556de09076c6ba8e999d
NozadzeOtto <nozadzeoto3@gmail.com>
2023-06-11 13:30:41 +0400

    task1

--- Assignment5/Task1/Program.cs
+++ Assignment5/Task1/Program.cs
@@ -1,34 +1,35 @@
 ﻿int[] array = new int[5] {3,-55555,4,4,5,};
 int index = 1;
-
 Console.WriteLine(arrayIndexItemSum(array, index));
 
+
+
+
+// sums number's items
 int arrayIndexItemSum(int[] arr, int index)
 {
     int sumedValue = 0;
     //tempVariable = Math.Abs(arr[index]);
     int tempVariable = arr[index];
     if (arr[index] < 0) tempVariable *= -1;
+    
     int divide = 1;
     for (int i = 1; i < getLength(tempVariable); i++){
         divide *= 10;
     }
     Console.WriteLine($" =  {divide}");
 
+    // sum items
     while (divide > 0)
     {
         sumedValue += tempVariable / divide;
         tempVariable %= divide;
         divide /= 10; ;
     }
-
-    //foreach (int i in temp)
-    //{
-    //    Console.WriteLine(i);
-    //}
     return sumedValue;
 }
 
+// gets Length of the Number : 567 -> 3
 int getLength(int number)
 {
     int length = 0;
@@ -39,5 +40,4 @@ int getLength(int number)
         length++;
     }
     return length;
-
 }
