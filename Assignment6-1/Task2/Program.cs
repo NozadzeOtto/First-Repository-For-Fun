407d346ee43bc3621fa7aa4e2eaebd5b247a1042
NozadzeOtto <nozadzeoto3@gmail.com>
2023-06-17 13:59:48 +0400

    "Finnished Task6"

--- Assignment6-1/Task2/Program.cs
+++ Assignment6-1/Task2/Program.cs
@@ -28,16 +28,18 @@ internal class Program
         Person person4 = new Person { FirstName = "Otto4", LastName = "Nozadze4", Age = 24 };
         Person person5 = new Person { FirstName = "Otto5", LastName = "Nozadze5", Age = 25 };
         Person person6 = new Person { FirstName = "Otto6", LastName = "Nozadze6", Age = 26 };
+
         // AddElement
         personList.AddElement(person1);
         personList.AddElement(person2);
         personList.AddElement(person3);
+
         // Count
         Console.WriteLine("Count: " + personList.Count); // 3
+
         // RemoveEllement
         personList.RemoveElement(person1);
         Console.WriteLine("Count: " + personList.Count); // 2
-
 
         // AddList 
         CustomList<Person> personList2 = new CustomList<Person>();
@@ -57,15 +59,43 @@ internal class Program
 
         // InsertElement
         personList.InsertElement(2, person4);
-        personList.GetList(0, personList.Count, out getList);
+
+        // InsertList
+        personList.InsertList(personList.Count-1, personList2);
+
+        // Full List
         Console.Write("Full List: ");
-        PrintPersonList(getList);
+        PrintPersonList(personList);
+
+        // RemoveList
+        personList2.AddList(personList);
+        Console.Write("\nBefore Remove:  ");
+        PrintPersonList(personList2);
+        personList2.RemoveList(2, 3);
+        Console.Write("After Remove:   ");
+        PrintPersonList(personList2);
+        
+        // Clear
+        personList2.Clear();
+        Console.Write("\nFull List2 after Clear: "); 
+        PrintPersonList(personList2);
 
         // GetElement
         personList.GetElement(2, out Person result1);
-        Console.WriteLine( "\nGetElement [2] " + result1.FirstName);
+        Console.WriteLine( "\n GetElement [2] " + result1.FirstName);
+
+        // Find
+        Console.WriteLine( "Find Person4: " + personList.Find(person4).FirstName);
 
 
+
+        CustomList<Organization> organizationList = new CustomList<Organization>();
+        Organization org1 = new Organization { Name = "TBC"};
+        Organization org2 = new Organization { Name = "BOG" };
+        organizationList.AddElement(org1);
+        organizationList.AddElement(org2);
+        Console.WriteLine();
+        PrintOrganizationList(organizationList);
 
     }
 
@@ -74,6 +104,15 @@ internal class Program
         for (int i = 0; i < list.Count; i++)
         {
             Console.Write(list[i].FirstName + "  ");
+        }
+        Console.WriteLine();
+    }
+
+    private static void PrintOrganizationList(CustomList<Organization> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.Write(list[i].Name + "  ");
         }
         Console.WriteLine();
     }
