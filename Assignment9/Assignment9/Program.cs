1ec99d8432bd888c0a8cee4fe5da487bd5d21e23
NozadzeOtto <nozadzeoto3@gmail.com>
2023-06-21 13:54:12 +0400

    Assignment 9 starting

---
+++ Assignment9/Assignment9/Program.cs
@@ -0,0 +1,89 @@
+﻿using Assignment9;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+
+        void CreateCombatVehicle()
+        {
+            Combat combat = new Combat();
+            combat.GetInfo();
+            combat.Models = getModels();
+            Console.WriteLine("Shoots? Y/N");
+            if (Console.ReadLine().ToLower() == "y") combat.Shoots = true;
+            combat.PrintInfo();
+            combat.PrintDescription();
+        }
+        void CreateSportVehicle()
+        {
+            Sport sport= new Sport();
+            sport.GetInfo();
+            sport.Models = getModels();
+            Console.WriteLine("input horse power:");
+            sport.horsePower = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input top speed:");
+            sport.TopSpeed = int.Parse(Console.ReadLine());
+            sport.PrintInfo();
+            sport.PrintDescription();
+        }
+
+
+        CreateCombatVehicle();
+        void CreateVehicle(int type)
+        {
+            switch (type)
+            {
+                case 1: CreateCombatVehicle(); break;
+            }
+            Console.WriteLine();
+        }
+
+
+
+
+
+
+
+        static int ReadInt(int min, int max)
+        {
+            int result;
+            while (!int.TryParse(Console.ReadLine(), out result) || result < min || result > max) {
+                Console.WriteLine($"Wrong number, input between {min} and {max}");
+            }
+            return result;
+        }
+
+        static Models getModels()
+        {
+            int number = 0;
+            Console.WriteLine("Choose model: ");
+            foreach (Models i in Enum.GetValues(typeof(Models)))
+            {
+                Console.Write($"{number}.{i} ");
+                number++;
+            }
+            Console.WriteLine();
+
+            int modelNumber = ReadInt(0, 7);
+
+            switch (modelNumber)
+            {
+                case 1: return Models.BMW; 
+                case 2: return Models.Honda; 
+                case 3: return Models.Ford; 
+                case 4: return Models.Honda;
+                case 5: return Models.Lamborgini;
+                case 6: return Models.Ferrari;
+                case 7: return Models.Mercedes_Benz;
+                default: return Models.Undefined;
+            }
+        }
+
+    }
+}
+
+
---
+++ Assignment9/Assignment9/Vehicles.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment9
+{
+    interface IDescription
+    {
+        void PrintDescription();
+    }
+    public abstract class Vehicles
+    {
+        public string Name { get; set; }
+        public Models Models { get; set; }
+        public abstract void GetInfo();
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Model: {Models.ToString()}");
+        }
+    }
+
+
+    public class Combat : Vehicles, IDescription
+    {
+        public bool Shoots { get; set; }
+        public override void GetInfo()
+        {
+            Console.WriteLine("Choose combat vehicle:");
+            Console.WriteLine("Tank");
+            Console.WriteLine("Armoured Car");
+            Name = Console.ReadLine();
+        }
+        public void PrintDescription()
+        {
+            Console.WriteLine("Combat vehicles are used for wars.");
+        }
+
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($"Shoots: {Shoots}");
+        }
+    }
+
+    public class Sport : Vehicles, IDescription
+    {
+        public int TopSpeed{ get; set; }
+        public int horsePower { get; set; }
+        public override void GetInfo()
+        {
+            Console.WriteLine("Choose combat vehicle:");
+            Console.WriteLine("F1");
+            Console.WriteLine("SuperCar");
+            Console.WriteLine("RallyCar");
+            Name = Console.ReadLine();
+        }
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($"Horse power: {horsePower}");
+            Console.WriteLine($"Top speen: {TopSpeed}");
+        }
+        public void PrintDescription()
+        {
+            Console.WriteLine("Sport vehicles are used for racing.");
+        }
+    }
+
+    public class Public : Vehicles, IDescription
+    {
+        public int PassengerAmount { get; set; }
+        public override void GetInfo()
+        {
+            Console.WriteLine("Choose combat vehicle:");
+            Console.WriteLine("Bus");
+            Console.WriteLine("Trum");
+            Name = Console.ReadLine();
+        }
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($"Passenger amount: {PassengerAmount}");
+        }
+        public void PrintDescription()
+        {
+            Console.WriteLine("Public vehicles are used public transport.");
+        }
+    }
+
+    public class Personal : Vehicles, IDescription
+    {
+        public int PassengerAmount { get; set; }
+        public override void GetInfo()
+        {
+            Console.WriteLine("Choose combat vehicle:");
+            Console.WriteLine("Bus");
+            Console.WriteLine("Trum");
+            Name = Console.ReadLine();
+        }
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($"Passenger amount: {PassengerAmount}");
+        }
+        public void PrintDescription()
+        {
+            Console.WriteLine("Personal vehicles are used family or personal use.");
+        }
+    }
+
+    public enum Models
+    {
+        Undefined = 0,
+        BMW = 1,
+        Honda = 2,
+        Ford = 3,
+        Hyunday = 4,
+        Lamborgini = 5,
+        Ferrari = 6,
+        Mercedes_Benz = 7,
+    }
+}
