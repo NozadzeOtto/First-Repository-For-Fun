b876ed5174a5ee323a949235fdfe3cc1a1319d95
NozadzeOtto <nozadzeoto3@gmail.com>
2023-06-28 16:57:29 +0400

    starting Assignment 11

---
+++ Assignment11/Task1/Calc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Calc<T> : IGeneric<T>
+    {
+        public T Add(T item, T item2)
+        {
+            if (item == null || item2 == null) throw new ArgumentNullException("Null Type");
+            T result = item + item2;
+
+        }
+
+        public T Multiply(T item, T item2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Substract(T item, T item2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assignment11/Task2/Storage.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Storage<T>
+    {
+        private T[] storage;
+        private int length;
+        public Storage()
+        {
+            storage = new T[1];
+            length = 0;
+        }
+
+        public int count
+        {
+            get { return length; }
+        }
+
+        public T this[int index]
+        {
+            get 
+            { 
+                if (index < 0 || index >= length) { throw new IndexOutOfRangeException("Index out of range."); }
+                return storage[index];
+            }
+            set 
+            {
+                if (index < 0 || index >= length) { throw new IndexOutOfRangeException("Index out of range."); }
+                storage[index] = value;
+            }
+
+        }
+
+        public void Add(T item)
+        {
+            if(length >= storage.Length) 
+            {
+                ResizeStorage(length * 2);
+            }
+            storage[length] = item;
+            length++;
+        }
+
+        public void UpdateElement(T item, T newItem) 
+        {
+            int index = Array.IndexOf(storage, item);
+            if (index == -1) throw new Exception ("Can't find item '" + item + "' in storage");
+            
+            if(newItem == null)
+            {
+                throw new ArgumentNullException("Argument is null.");
+            }
+            storage[index] = newItem;
+        }
+
+        public void Remove(T item) 
+        {
+            int index = Array.IndexOf(storage, item);
+            if (index == -1) throw new Exception("Can't find item '" + item + "' in storage");
+            
+            if (item == null)
+            {
+                throw new ArgumentNullException("Argument is null.");
+            }
+
+            for (int i = index ; i < length-1; i++)
+            {
+                storage[i] = storage[i + 1];
+            }
+            length--;
+            storage[length] = item;
+        }
+
+        public void Clear()
+        {
+            ResizeStorage(0);
+            length = 1;
+        }
+
+        private void ResizeStorage(int size)
+        {
+            Array.Resize(ref storage, size);
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+
+            for (int i = 0; i < length; i++)
+            {
+                result += storage[i].ToString() + ", ";
+            }
+            return result;
+        }
+    }
+}
