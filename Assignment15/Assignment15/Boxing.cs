64a55b7471d2d244fe059ddd76a6e51c68c53d25
NozadzeOtto <nozadzeoto3@gmail.com>
2023-07-11 21:25:12 +0400

    Assignment 15

---
+++ Assignment15/Assignment15/Boxing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment15
+{
+    public class Boxing
+    {
+        public void StartRound(string opponent1, string opponent2)
+        {
+            Console.WriteLine("Starting round between {0} and {1}", opponent1, opponent2);
+        }
+
+        public void EndRound(string opponent1, string opponent2)
+        {
+            Console.WriteLine("Ending round between {0} and {1}", opponent1, opponent2);
+        }
+
+        public void WhosWinner(string winnerName)
+        {
+            Console.WriteLine("Winner is " + winnerName);
+        }
+    }
+}
---
+++ Assignment15/Assignment15/MikeTyson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment15
+{
+    public class MikeTyson
+    {
+        public static void Punch(string punchWeight)
+        {
+            Console.WriteLine($"{punchWeight} is nothing for mike!");
+            Console.WriteLine("Mike Tyson punch could easily killed opponent :O");
+        }
+        public static void BiteEar(string opponentName, string whichSideEar)
+        {
+            Console.WriteLine($"{opponentName} does not have {whichSideEar} ear enymore :D");
+        }
+
+    }
+}
