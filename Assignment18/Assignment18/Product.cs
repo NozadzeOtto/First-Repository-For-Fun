a1f1eda40df8784abd5effe194920e5a6e892aff
NozadzeOtto <nozadzeoto3@gmail.com>
2023-07-23 21:40:37 +0400

    starting Assignment 18

---
+++ Assignment18/Assignment18/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment18
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        public int ShopId { get; set; }
+
+    }
+}
